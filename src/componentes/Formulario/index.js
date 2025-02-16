fe286ca970f448cd62cd9c282813ff417aceee1f
MxSGameJPS <mxsgamejps@gmail.com>
2025-02-16 07:01:58 -0300

    formulario

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,64 @@
+import { useState } from 'react';
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log("Formulário submetido => ", nome, cargo, imagem, time);
+  };
+
+
+
   return (
     <section className="formulario">
-      <form>
-        <h2>Preencha os dados para criar o card dos espectadores</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite a URL da imagem" />
+      <form onSubmit={aoSalvar}>
+        <h2>Preencha os dados para criar o card dos alunos destaques</h2>
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={valor => setNome(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={valor => setCargo(valor)}
+        />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Digite a URL da imagem"
+          valor={imagem}
+          aoAlterado={valor => setImagem(valor)}
+        />
+        <ListaSuspensa 
+          required={true} 
+          label="Time" 
+          itens={times} 
+          valor={time} 
+          aoAlterado={valor => setTime(valor)} 
+        />
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );

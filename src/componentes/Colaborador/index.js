5c386377dde0c514fbf21f3ed7f4dfb46c995770
MxSGameJPS <mxsgamejps@gmail.com>
2025-02-20 11:10:15 -0300

    atualização

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,9 @@
+import { IoCloseCircle } from "react-icons/io5";
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
+        <IoCloseCircle className='deletar' onClick={aoDeletar}></IoCloseCircle>
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
